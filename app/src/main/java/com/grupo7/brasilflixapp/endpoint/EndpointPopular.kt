ff0f63a219cf55587a4d8eec4c7c6bb53f6884f6
lucas.coelho <lucas.coelho@marketup.com>
2021-08-16 15:46:12 -0300

    Filmes Populares implemented

---
+++ app/src/main/java/com/grupo7/brasilflixapp/endpoint/EndpointPopular.kt
@@ -0,0 +1,12 @@
+package com.grupo7.brasilflixapp.endpoint
+
+import com.grupo7.brasilflixapp.model.films.filmsResults
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface EndpointPopular {
+    @GET("movie/popular")
+    fun getPopular(@Query("page") page: Int ) : Call<filmsResults>
+
+}
--- app/src/main/java/com/grupo7/brasilflixapp/fragments/popular/popularFragment.kt
+++ app/src/main/java/com/grupo7/brasilflixapp/fragments/popular/popularFragment.kt
@@ -5,13 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.grupo7.brasilflixapp.R
+import com.grupo7.brasilflixapp.adapter.films.filmsAdapter
+import com.grupo7.brasilflixapp.adapter.popular.popularAdapter
+import com.grupo7.brasilflixapp.databinding.FragmentHomeBinding
+import com.grupo7.brasilflixapp.databinding.FragmentPopularBinding
+import com.grupo7.brasilflixapp.endpoint.Endpoint
+import com.grupo7.brasilflixapp.endpoint.EndpointPopular
+import com.grupo7.brasilflixapp.model.films.films
+import com.grupo7.brasilflixapp.model.films.filmsResults
+import com.grupo7.brasilflixapp.util.api.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class popularFragment : Fragment() {
 
+    private var binding: FragmentPopularBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //------------------ API - Popular -------------------------//
+        val retrofitClient = RetrofitInstance
+            .getRetrofitInstance("https://api.themoviedb.org/3/")
+        val endpoint = retrofitClient.create(EndpointPopular::class.java)
+        val callback = endpoint.getPopular(1)
+        callback.enqueue(object : Callback<filmsResults> {
+            override fun onFailure(call: Call<filmsResults>, t: Throwable) {
+                Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<filmsResults>, response: Response<filmsResults>) {
+                showPopularFilms(response.body()!!.results)
+            }
+        })
 
     }
 
@@ -19,9 +50,25 @@ class popularFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_popular, container, false)
+        binding = FragmentPopularBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    private fun showPopularFilms(filmsList: List<films>) {
+        filmsList.forEach {
+            val filmeAdapter = popularAdapter(filmsList)
+            binding?.let {
+                with(it) {
+                    popularRecyclerView.layoutManager = LinearLayoutManager(context)
+                    popularRecyclerView.adapter = filmeAdapter
+                }
+            }
+        }
+    }
 
 }
