98c90ee9864bdd95394cbb1894b53f3e370c85bc
Renan Corredato <renancorredato@hotmail.com>
2021-08-18 13:08:03 -0300

    feature/xml Alterar Email

--- app/src/main/java/com/grupo7/brasilflixapp/activity/Detail/DetailActivity.kt
+++ app/src/main/java/com/grupo7/brasilflixapp/activity/Detail/DetailActivity.kt
@@ -3,10 +3,15 @@ package com.grupo7.brasilflixapp.activity.Detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.grupo7.brasilflixapp.R
+import com.grupo7.brasilflixapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
