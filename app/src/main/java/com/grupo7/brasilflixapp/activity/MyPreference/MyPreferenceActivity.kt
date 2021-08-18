885b662f0d08d810fe70a8f51b05d6a08eda5932
Renan Corredato <renancorredato@hotmail.com>
2021-08-18 16:49:54 -0300

    feature/xml Favoritos

---
+++ app/src/main/java/com/grupo7/brasilflixapp/activity/MyPreference/MyPreferenceActivity.kt
@@ -0,0 +1,12 @@
+package com.grupo7.brasilflixapp.activity.MyPreference
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupo7.brasilflixapp.R
+
+class MyPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_preference)
+    }
+}
---
+++ app/src/main/java/com/grupo7/brasilflixapp/fragments/MyPreference/MyPreferenceFragment.kt
@@ -0,0 +1,21 @@
+package com.grupo7.brasilflixapp.fragments.MyPreference
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.grupo7.brasilflixapp.R
+
+class MyPreferenceFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_preference, container, false)
+    }
+
+}
