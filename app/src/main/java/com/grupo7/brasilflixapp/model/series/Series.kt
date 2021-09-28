84ffc26e3153c47836724f55a84fec64bcdb97a3
Lucas Esteves <lucas.coelho9280@gmail.com>
2021-09-27 23:47:12 -0300

    All Series Database Implemented

--- app/src/main/java/com/grupo7/brasilflixapp/model/series/Series.kt
+++ app/src/main/java/com/grupo7/brasilflixapp/model/series/Series.kt
@@ -6,21 +6,21 @@ import com.grupo7.brasilflixapp.model.films.films
 
 class Series (
     @SerializedName("poster_path")
-    var poster_path : String? = null,
+    var poster_path : String?,
     @SerializedName("first_air_date")
-    var first_air_date : String? = null,
+    var first_air_date : String?,
     @SerializedName("original_name")
-    var original_name : String? = null,
+    var original_name : String?,
     @SerializedName("vote_average")
-    var vote_average: Double? = null,
+    var vote_average: Double?,
     @SerializedName("backdrop_path")
-    var backdrop_path: String? = null,
+    var backdrop_path: String?,
     @SerializedName("id")
-    val id: Int? = null,
+    val id: Int?,
     @SerializedName("title")
-    var title: String? = null,
+    var title: String?,
     @SerializedName("overview")
-    val overview: String? = null
+    val overview: String?
 
     ) {
 
